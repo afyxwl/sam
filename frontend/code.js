991df4b7a2b82b8adc7da013cc84a39261b202a0
Maxim Hrabovi <dechioyo@gmail.com>
2025-11-21 09:51:15 +0200

    Posting is supported

--- frontend/code.js
+++ frontend/code.js
@@ -1,50 +1,71 @@
+let server = "http://localhost:5096/";
+
 let i = 1.0; 
 let date = new Date();
-const { createElement } = require("react");
+//const { createElement } = require("react");
 
 function myfunc(){
     console.log("rizz");
 }
-function post(){
-    const xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://localhost:5096/posts/");
-        xhr
-        .setRequestHeader("Content-Type", "application/json");
-        const body = JSON
-        .stringify(
-        {
-            userId: 1,
-            title: "Demo Todo Data using XMLHttpRequest",
-            completed: false,
-        });
-        xhr
-        .onload = () => 
-        {
-            if (xhr.readyState == 4 && xhr.status == 201) 
-            {
-                console.log(JSON.parse(xhr.responseText));
-            } else 
-            {
-                console.log(`Error: ${xhr.status}`);
-            }
-        };
-        xhr.send(body);
+function post(text, name, tmdbID,stars){
+    try{
+    var get = fetch(server+"reviews", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify({displayName: name, text: text, tmdbId: tmdbID,rating: stars}),
+    })
+    ;
+    return get
+        .then((response) => {
+            if (response.status === 200) return 200;
+            if (response.status === 400) return 400;
+            if (response.status === 404) return 404;
+            return response.status;
+        })
+        .catch((error) => { console.error(error); throw error; });
+}
+    catch(error){alert(error.message);}
 }
 function getall(){
-<<<<<<< HEAD
-    fetch("http://localhost:5096/reviews/",{
-=======
-    fetch("http://10.216.208.288:5096/reviews/",{
->>>>>>> 024b771 (?)
+  //  try{
+  var a;
+    fetch(server + "reviews", {
         method: "GET",
         headers: {
-    "Content-type": "application/json; charset=UTF-8"}
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        
     })
     .then((response) => response.json())
-    .then((json) => console.log(json));
-}
-function newpost(name, stars, text, tmdb_id, tmbd_name){
+    .then((data) => {
+        
+        for(let i = 0.0; i < data.length; i++){
+            var dto = data[i];
+            const dtodate = new Date(data[i].createdAt);
+            newpost(dto.displayName,dto.rating,dto.text,dto.tmdbId,dto.movieName,dto.posterUrl,dtodate.toLocaleString("uk-UA",{timezone: "UTC+3"}));
+        }
+
+    })
+    .catch((error) => {
+        console.log("error", error);
+    });
     
+//}
+   // catch(error){console.log("error");}   
+    //var a = json.Parse();
+    //for(let i = 0; i < a.length; i++){
+    //    newpost(a[i].displayName,a[i].rating,a[i].text,a[i].tmdbID,a[i]);
+    //}
+}
+function newpost(name, stars, text, tmdb_id, tmbd_name,image_url, created_at){
+    
+    if(stars > 10 || stars < 1){ alert("Кількість зірочок підтримується у значеннях від 1 до 10."); return;}
+    if(!name){ alert("Ім'я пусте."); return;}
+    if(!text){ alert("Текст посту пустий."); return;}
+    if(!tmdb_id){ alert("Поле з tmdb ID пусте."); return;}
+
 
     const div = document.createElement("div");
     div.className = "post";
@@ -53,7 +74,7 @@ function newpost(name, stars, text, tmdb_id, tmbd_name){
         
     //img
     const img = document.createElement("img");
-    img.src = "noimage.png";
+    img.src = image_url;
     img.className = "poster";
     document.getElementById(i).appendChild(img);
 
@@ -65,7 +86,7 @@ function newpost(name, stars, text, tmdb_id, tmbd_name){
 
         const author_span = document.createElement("span");
         author_span.className = "author_date";
-        author_span.innerHTML = "Пише " + name + ", " + date.toLocaleDateString() + ",";
+        author_span.innerHTML = "Пише " + name + ", " + created_at + ",";
         document.getElementById(i).getElementsByClassName("post_text")[0].appendChild(author_span);
 
         const star_view = document.createElement("div");
@@ -92,18 +113,34 @@ function newpost(name, stars, text, tmdb_id, tmbd_name){
         document.getElementById(i).getElementsByClassName("post_text")[0].appendChild(talking_about);
 
         const movie = document.createElement("a");
-        movie.href = "https://themoviedb.org/" + tmdb_id + "/";
+        movie.href = "https://themoviedb.org/movie/" + tmdb_id + "/";
         movie.innerHTML = tmbd_name;
         document.getElementById(tmdb_id+"_"+i).appendChild(movie);
     i++;
 }
 
 function buttonPress(){
-    newpost(
-        document.getElementById("iname").value,
-        document.getElementById("istar").value,
+    try{
+    var a = post(
         document.getElementById("ipost").value,
+        document.getElementById("iname").value,
         document.getElementById("itmdb").value,
-        "unknown"
+        document.getElementById("istar").value,
     );
+    a.then((status) => {
+        if (status === 400 || status === 404) {
+            alert("Відбулась помилка. Спробуйте інший запит.\nКод помилки: " + status); return;
+        }
+        else{
+    alert("Пост опубліковано. \nНатисніть щоб перезавантажити сторінку.")
+
+            location.reload();
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+    
+    
+}
+    catch(error){ alert(error);}
 }
